2a478dc5784a09b751aab1d91795072c2d3056e6
sjYoondeltar <dbsdns@naver.com>
2022-10-04 20:43:57 +0900

    fix the test script params error

--- examples/test_deep_value_mpc.py
+++ examples/test_deep_value_mpc.py
@@ -186,7 +186,7 @@ if __name__ == '__main__':
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    rsmpc = set_ctrl()
+    rsmpc = set_ctrl(params)
     
     test_process()
     
