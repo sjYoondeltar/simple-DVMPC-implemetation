7252a4d799cc83d618a574d0ca44c0f505b934c8
sjYoondeltar <dbsdns@naver.com>
2022-10-21 17:02:46 +0900

    ensemble value net class 추가

--- controller/value_net_utils.py
+++ controller/value_net_utils.py
@@ -44,6 +44,25 @@ class ValueNet(nn.Module):
         return q_value
 
 
+class EnsembleValueNet(nn.Module):
+    def __init__(self, state_size, hidden_size, num_ensemble):
+        super().__init__()
+
+        self.fc1 = nn.Linear(state_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, num_ensemble)
+
+    def forward(self, states):
+        
+        x1 = F.leaky_relu(self.fc1(states))
+        
+        x1 = F.leaky_relu(self.fc2(x1))
+
+        q_value = self.fc3(x1)
+
+        return q_value
+
+
 class ExperienceReplayMemory:
     def __init__(self, buffer_size):
         self.buffer_size = buffer_size
