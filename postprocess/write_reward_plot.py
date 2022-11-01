c3ad4058b9d51755c6610c98dabceaac962fcc95
sjYoondeltar <dbsdns@naver.com>
2022-11-01 15:06:51 +0900

    figure plotter 수정

--- postprocess/write_reward_plot.py
+++ postprocess/write_reward_plot.py
@@ -40,7 +40,7 @@ class RewardPlotter(object):
         
     def save_reward_plot(self, file_path_list):
         
-        fig = plt.figure(figsize=(40, 20))
+        fig = plt.figure(figsize=(20, 10))
         
         ax1 = fig.add_subplot(2, 1, 1)
         
@@ -50,19 +50,21 @@ class RewardPlotter(object):
         
             rewards_per_episode, is_reach_per_episode = self.calc_results(file_path)
             
-            ax1.plot(rewards_per_episode, label=file_path.split('/')[2])
+            model_name_list = file_path.split('/')
+            
+            ax1.plot(rewards_per_episode, label=model_name_list[1] + "+" + model_name_list[2])
         
             ax2.plot(is_reach_per_episode)
         
         ax1.set_ylabel("cumulative rewards")
         
         ax1.grid()
-        ax1.legend()
-        ax1.set_xlim([0, 100])
+        ax1.legend(loc='upper left')
+        ax1.set_xlim([0, 40])
         
-        ax2.set_ylabel("success rate")
+        ax2.set_ylabel("success to reach a goal")
         ax2.set_xlabel("episode")
-        ax2.set_xlim([0, 100])
+        ax2.set_xlim([0, 40])
         ax2.grid()
         
         plt.draw()
@@ -76,9 +78,9 @@ if __name__ == '__main__':
     
     logs_list = [
         "runs/value_net/cem_dense/logs/20221030_222322.csv",
-        "runs/value_net/cem_sparse/logs/20221031_225509.csv",
+        # "runs/value_net/cem_sparse/logs/20221031_225509.csv",
         "runs/ensemble_value_net/mppi_dense/logs/20221031_005341.csv",
-        "runs/ensemble_value_net/mppi_sparse/logs/20221031_132405.csv"
+        # "runs/ensemble_value_net/mppi_sparse/logs/20221031_132405.csv"
     ]
     
     reward_plot_draw.save_reward_plot(logs_list)
